8ddf8b7e7daa9b8f71902ea6c3524adb1bc6d3d8
Mohsen Akhlaghi <persisch@msn.com>
2021-10-20 20:34:13 +0300

    jsonSerialize fix php8 interface compatibility

--- src/Scope.php
+++ src/Scope.php
@@ -293,9 +293,9 @@ class Scope implements \JsonSerializable
     }
 
     /**
-     * @return array
+     * @return mixed
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return $this->toArray();
     }
